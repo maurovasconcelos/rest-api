989a8980cf05d094528b9e817eec7d04be9534b7
Mauro Vasconcelos <maurovasconcelos@criasol.com.br>
2023-05-28 22:04:42 -0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ServiceOrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::post('/service-orders', [ServiceOrderController::class, 'create']);
+Route::get('/service-orders', [ServiceOrderController::class, 'index']);
